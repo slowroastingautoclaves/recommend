9e6888bec97b43c4fb27e61f647fa98ebd1d23ff
Joshua Jibilian <jwjibilian@gmail.com>
2016-01-28 16:51:47 -0500

    Coded person 3 and added name to AntLabMain

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -42,7 +42,10 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+	  String toReturn = "";
+		for (int i = input.length() - 1; i >= 0; i --){
+			toReturn += input.charAt(i);
+		}
+		return toReturn;
 	}
 }
